7b4f96352737c13652ab52bae896f8a49ef18118
joethyr <joethayr@gmail.com>
2021-08-30 19:42:34 -0600

    Made height of the tower dynamic.

--- tower_of_hanoi.rb
+++ tower_of_hanoi.rb
@@ -1,11 +1,23 @@
 class TowerHanoi
-  def initialize
-    @columns = { 1 => ["o"], 2 => ["oo"], 3 => %w[ooo oooo] }
+  def initialize(number_disks)
+    @number_disks = number_disks
+    @columns = [[], [], []]
+    @winning_array = []
+    generate_disks
+  end
+
+  def generate_disks
+    count_disk = ''
+    @number_disks.times do
+      count_disk += "o"
+      @winning_array <<  count_disk
+      @columns[rand(3)].unshift(count_disk)
+    end
   end
 
   def display_tower
     3.times do |i|
-      puts @columns.fetch_values(i + 1)
+      puts @columns[i]
       puts "Column #{i + 1}"
       puts
     end
@@ -16,11 +28,11 @@ class TowerHanoi
     print ">"
     disk_from = gets.chomp.to_i
     if [1, 2, 3].any? { |num| num == disk_from}
-      @value_from = @columns[disk_from].shift
+      @value_from = @columns[disk_from - 1].shift
       puts "The disk has now been removed from Column #{disk_from}."
     else
       puts "The entered Column number is not valid."
-      remove_disk
+      return remove_disk
     end
   end
 
@@ -29,24 +41,23 @@ class TowerHanoi
     print ">"
     disk_to = gets.chomp.to_i
     if [1, 2, 3].any? { |num| num == disk_to}
-      @columns.fetch(disk_to).unshift(@value_from)
+      @columns.fetch(disk_to - 1).unshift(@value_from)
     else
       puts "The entered Column number is not valid."
-      move_disk
+      return move_disk
     end
     puts
   end
 
   def game
-    until @columns.value?(%w[o oo ooo oooo])
+    until @columns.include? @winning_array
       display_tower
       remove_disk
       move_disk
     end
-    display_tower
-    puts "You Win!!!"
+    puts "you win!"
   end
 end
 
-play = TowerHanoi.new
+play = TowerHanoi.new(3)
 play.game
