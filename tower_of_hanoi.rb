45547ff22906d20bc9147c02031866d0a217ab67
joethyr <joethayr@gmail.com>
2021-08-31 19:29:06 -0600

    finished task

--- tower_of_hanoi.rb
+++ tower_of_hanoi.rb
@@ -15,7 +15,7 @@ class TowerHanoi
     end
   end
 
-  def display_tower
+  def render
     3.times do |i|
       puts @columns[i]
       puts "Column #{i + 1}"
@@ -23,13 +23,26 @@ class TowerHanoi
     end
   end
 
+  def game
+    until @columns.include? @winning_array
+      render
+      remove_disk
+      move_disk
+    end
+    render
+    puts "Congratulations! You win!"
+  end
+
   def remove_disk
     puts "Enter the column number of the disk you want to move:"
     print ">"
-    disk_from = gets.chomp.to_i
-    if [1, 2, 3].any? { |num| num == disk_from}
-      @value_from = @columns[disk_from - 1].shift
+    disk_from = gets.chomp
+    if [1, 2, 3].any? { |num| num == disk_from.to_i}
+      @value_from = @columns[disk_from.to_i - 1].shift
       puts "The disk has now been removed from Column #{disk_from}."
+    elsif disk_from == "quit"
+      puts "Goodbye!"
+      exit
     else
       puts "The entered Column number is not valid."
       return remove_disk
@@ -40,24 +53,18 @@ class TowerHanoi
     puts "Please enter which row you will put the disk in?"
     print ">"
     disk_to = gets.chomp.to_i
-    if [1, 2, 3].any? { |num| num == disk_to}
-      @columns.fetch(disk_to - 1).unshift(@value_from)
+    if [1, 2, 3].any? { |num| num == disk_to.to_i }
+      @columns.fetch(disk_to.to_i - 1).unshift(@value_from)
+    elsif disk_to == "quit"
+      puts "Goodbye!"
+      exit
     else
       puts "The entered Column number is not valid."
       return move_disk
     end
     puts
   end
-
-  def game
-    until @columns.include? @winning_array
-      display_tower
-      remove_disk
-      move_disk
-    end
-    puts "you win!"
-  end
 end
 
-play = TowerHanoi.new(3)
+play = TowerHanoi.new(4)
 play.game
